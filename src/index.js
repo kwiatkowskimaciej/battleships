e62b2e7e5a22bd183e5583922e0bcd60c5bf4076
maciejkwiatkowski <maciek.kwiatkowski@outlook.com>
2024-03-26 00:02:36 +0100

    feat(main-game-loop) Add GameController() to manage game state

--- src/index.js
+++ src/index.js
@@ -1,11 +1,38 @@
 import './style.css';
+import Player from './classes/player';
+import Gameboard from './classes/gameboard';
+import Ship from './classes/ship';
 
-function component() {
-  const element = document.createElement('div');
+function GameController(
+  playerOneName = 'Player One',
+  playerTwoName = 'PlayerTwo'
+) {
+  const gameboardOne = new Gameboard();
+  const gameboardTwo = new Gameboard();
 
-  element.innerHTML = 'Hello, world!';
+  const playerOne = new Player(playerOneName, gameboardOne);
+  const playerTwo = new Player(playerTwoName, gameboardTwo);
 
-  return element;
+  let activePlayer = playerOne;
+  const getActivePlayer = () => activePlayer;
+
+  const switchPlayerTurn = () => {
+    activePlayer = activePlayer === playerOne ? playerTwo : playerTwo;
+  };
+
+  const placeShips = () => {
+    gameboardOne.placeShip(new Ship(1), [0, 0], 'horizontal');
+    gameboardTwo.placeShip(new Ship(1), [0, 0], 'horizontal');
+  };
+
+  placeShips();
+
+  const playRound = (coordinates) => {
+    try {
+      activePlayer.attack(coordinates);
+      switchPlayerTurn();
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
 }
-
-document.body.appendChild(component());

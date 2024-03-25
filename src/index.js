7faf9f7dab8d37fc3a25f858e1dac128348b6ed2
maciejkwiatkowski <maciek.kwiatkowski@outlook.com>
2024-03-26 00:28:34 +0100

    feat(main-game-loop): Add ScreenControler() to manage DOM interactions

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ function GameController(
 ) {
   const gameboardOne = new Gameboard();
   const gameboardTwo = new Gameboard();
+  const getGameboardOne = () => gameboardOne;
+  const getGameboardTwo = () => gameboardTwo;
 
   const playerOne = new Player(playerOneName, gameboardOne);
   const playerTwo = new Player(playerTwoName, gameboardTwo);
@@ -35,4 +37,54 @@ function GameController(
       console.error(error.message);
     }
   };
+
+  return {
+    getGameboardOne,
+    getGameboardTwo,
+    getActivePlayer,
+    playRound,
+  };
 }
+
+function ScreenController() {
+  const game = GameController();
+  const gameboardOneElement = document.querySelector('.gameboardOne');
+  const gameboardTwoElement = document.querySelector('.gameboardTwo');
+
+  const updateScreen = () => {
+    gameboardOneElement.innerHTML = '';
+    gameboardTwoElement.innerHTML = '';
+
+    const gameboardOne = game.getGameboardOne();
+    const gameboardTwo = game.getGameboardTwo();
+    const activePlayer = game.getActivePlayer();
+
+    gameboardOne.board.forEach((row, rowIndex) => {
+      row.forEach((target, targetIndex) => {
+        const targetButton = document.createElement('button');
+        targetButton.classList.add('target');
+        targetButton.dataset.x = targetIndex;
+        targetButton.dataset.y = rowIndex;
+        targetButton.textContent = ' ';
+        if (target instanceof Ship) targetButton.classList.add('ship');
+        gameboardOneElement.appendChild(targetButton);
+      });
+    });
+
+    gameboardTwo.board.forEach((row, rowIndex) => {
+      row.forEach((target, targetIndex) => {
+        const targetButton = document.createElement('button');
+        targetButton.classList.add('target');
+        targetButton.dataset.x = targetIndex;
+        targetButton.dataset.y = rowIndex;
+        targetButton.textContent = ' ';
+        if (target instanceof Ship) targetButton.classList.add('ship');
+        gameboardTwoElement.appendChild(targetButton);
+      });
+    });
+  };
+
+  updateScreen();
+}
+
+ScreenController();

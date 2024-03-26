8a6d64a07615d53cf508d5f0a8c6c16ddc4a2e45
maciejkwiatkowski <maciek.kwiatkowski@outlook.com>
2024-03-27 00:17:06 +0100

    feat(main-game-loop): Add missed attack display

--- src/controllers/screenController.js
+++ src/controllers/screenController.js
@@ -2,57 +2,64 @@ import Ship from '../classes/ship';
 import GameController from './gameController';
 
 function ScreenController() {
-    const game = GameController();
-    const gameboardOneElement = document.querySelector('.gameboardOne');
-    const gameboardTwoElement = document.querySelector('.gameboardTwo');
-  
-    const updateScreen = () => {
-      gameboardOneElement.innerHTML = '';
-      gameboardTwoElement.innerHTML = '';
-  
-      const gameboardOne = game.getGameboardOne();
-      const gameboardTwo = game.getGameboardTwo();
-      const activePlayer = game.getActivePlayer();
-  
-      gameboardOne.board.forEach((row, rowIndex) => {
-        row.forEach((target, targetIndex) => {
-          const targetButton = document.createElement('button');
-          targetButton.classList.add('target');
-          targetButton.dataset.x = targetIndex;
-          targetButton.dataset.y = rowIndex;
-          targetButton.textContent = ' ';
-          if (target instanceof Ship) targetButton.classList.add('ship');
-          gameboardOneElement.appendChild(targetButton);
-        });
-      });
-  
-      gameboardTwo.board.forEach((row, rowIndex) => {
-        row.forEach((target, targetIndex) => {
-          const targetButton = document.createElement('button');
-          targetButton.classList.add('target');
-          targetButton.dataset.x = targetIndex;
-          targetButton.dataset.y = rowIndex;
-          targetButton.textContent = ' ';
-          if (target instanceof Ship) targetButton.classList.add('ship');
-          gameboardTwoElement.appendChild(targetButton);
-        });
-      });
-    };
-  
-    function boardClickHandler(e) {
-      const x = Number(e.target.dataset.x);
-      const y = Number(e.target.dataset.y);
-  
-      if (!x || !y) return;
-  
-      game.playRound([x, y]);
-      updateScreen();
+  const game = GameController();
+  const gameboardOneElement = document.querySelector('.gameboardOne');
+  const gameboardTwoElement = document.querySelector('.gameboardTwo');
+  const activePlayerElement = document.querySelector('.activePlayer');
+
+  const displayAttackResult = (gameboard, position, button) => {
+    const missedAttack = gameboard.missedAttacks.some((array) =>
+      array.every((value, index) => value === position[index])
+    );
+
+    if (missedAttack) {
+      button.classList.add('miss');
     }
-  
-    gameboardOneElement.addEventListener('click', boardClickHandler);
-    gameboardTwoElement.addEventListener('click', boardClickHandler);
-  
+  };
+
+  const displayGameboard = (gameboard, element) => {
+    gameboard.board.forEach((row, y) => {
+      row.forEach((cell, x) => {
+        const targetButton = document.createElement('button');
+        targetButton.classList.add('target');
+        targetButton.dataset.x = x;
+        targetButton.dataset.y = y;
+        targetButton.textContent = ' ';
+
+        displayAttackResult(gameboard, [y, x], targetButton);
+        if (cell instanceof Ship) targetButton.classList.add('ship');
+
+        element.appendChild(targetButton);
+      });
+    });
+  };
+
+  const updateScreen = () => {
+    gameboardOneElement.innerHTML = '';
+    gameboardTwoElement.innerHTML = '';
+    activePlayerElement.innerHTML = game.getActivePlayer().name;
+
+    const gameboardOne = game.getGameboardOne();
+    const gameboardTwo = game.getGameboardTwo();
+
+    displayGameboard(gameboardOne, gameboardOneElement);
+    displayGameboard(gameboardTwo, gameboardTwoElement);
+  };
+
+  function boardClickHandler(e) {
+    const x = Number(e.target.dataset.x);
+    const y = Number(e.target.dataset.y);
+
+    if (!x || !y) return;
+
+    game.playRound([y, x]);
     updateScreen();
   }
 
-  export default ScreenController;
+  gameboardOneElement.addEventListener('click', boardClickHandler);
+  gameboardTwoElement.addEventListener('click', boardClickHandler);
+
+  updateScreen();
+}
+
+export default ScreenController;

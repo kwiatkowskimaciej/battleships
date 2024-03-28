fc91d1fca7bf1d897f311f69249cd367b57acd41
maciejkwiatkowski <maciek.kwiatkowski@outlook.com>
2024-03-28 23:18:28 +0100

    refactor(ui): Move displayGameboard() to ui directory

---
+++ src/ui/displayGameboard.js
@@ -0,0 +1,33 @@
+import Ship from '../classes/ship';
+
+function displayGameboard(gameboard) {
+  const gameboardContainer = document.querySelector('.gameboardContainer');
+  const gameboardElement = document.createElement('div');
+  gameboardElement.classList.add('gameboard');
+
+  gameboard.board.forEach((row, y) => {
+    row.forEach((cell, x) => {
+      const targetElement = document.createElement('div');
+      targetElement.classList.add('target');
+      targetElement.dataset.x = x;
+      targetElement.dataset.y = y;
+      targetElement.textContent = ' ';
+
+      if (cell instanceof Ship) {
+        targetElement.classList.add('ship');
+      }
+      displayAttackResult(gameboard, [x, y], targetElement);
+
+      gameboardElement.appendChild(targetElement);
+    });
+  });
+
+  gameboardContainer.appendChild(gameboardElement);
+}
+
+function displayAttackResult(gameboard, coordinates, element) {
+  const attackResult = gameboard.getAttackResult(coordinates);
+  if (attackResult) element.classList.add(attackResult);
+}
+
+export default displayGameboard;
